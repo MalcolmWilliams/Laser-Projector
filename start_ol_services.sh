48b4e97a25f441992bd4163b394c56c8a041a4a0
malcolmwilliams <malcolmnwilliams@gmail.com>
2016-12-05 10:55:58 -0800

    before starting code cleanup

--- start_ol_services.sh
+++ start_ol_services.sh
@@ -4,10 +4,10 @@ echo "-----------Plug in the first lasershark (usb portXXX)-------------"
 read -p "Press 'Enter' when ready..." temp
 ./openlase/build/output/output & 
 ./openlase/build/tools/simulator &
-#./lasershark_hostapp/lasershark_jack &
+./lasershark_hostapp/lasershark_jack &
 sleep 1 
 echo "-----------Plug in the second lasershark (usb portXXX)------------"
 read -p "Press 'Enter' when ready..." temp
 ./openlase/build/output/output & 
 ./openlase/build/tools/simulator &
-#./lasershark_hostapp/lasershark_jack &
+./lasershark_hostapp/lasershark_jack &
